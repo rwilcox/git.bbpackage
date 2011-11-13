1703b7bc49b520bacc95e5c1361f431540fc2e8f
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-11-13 15:46:50 -0500

    *now* fully compatible with paths with spaces in them

--- Contents/Scripts/git/git_diff.sh
+++ Contents/Scripts/git/git_diff.sh
@@ -3,6 +3,6 @@
 export docpath="$(dirname "$BB_DOC_PATH")"
 
 
-cd "$docpath"; cd $(git rev-parse --show-toplevel); echo "** Git diff of repository at:"; pwd; echo
-cd "$docpath"; cd $(git rev-parse --show-toplevel); git diff
+cd "$docpath"; cd "$(git rev-parse --show-toplevel)"; echo "** Git diff of repository at:"; pwd; echo
+cd "$docpath"; cd "$(git rev-parse --show-toplevel)"; git diff
 
