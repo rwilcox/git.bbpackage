8b72866e1d8f4f838e58f6942aeafce828d41c70
Ryan Wilcox <rwilcox@wilcoxd.com>
2012-11-07 15:43:38 -0500

    added show OURs or THEIRs version of confilcted file script, reorg

---
+++ Contents/Scripts/git/Conflicts/show_OURs_version_of_current_conflicted_file.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+BASE_FOLDER=`dirname "$0"`
+SUPPORT_FOLDER="$BASE_FOLDER/../../../SharedSupport/"
+
+source "$SUPPORT_FOLDER/project_root.sh"
+source "$SUPPORT_FOLDER/absolute_to_relative_path.sh"
+
+cd "$PROJECT_ROOT"
+
+# Thanks <http://stackoverflow.com/a/167365/224334> !!!
+git show :2:`absolute_to_relative_path "$BB_DOC_PATH" "$PROJECT_ROOT"` > "$BB_DOC_PATH.conflict.ours"
+
+/usr/local/bin/bbedit "$BB_DOC_PATH.conflict.ours"
